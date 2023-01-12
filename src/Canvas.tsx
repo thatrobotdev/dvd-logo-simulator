abdc451718725568fef1d9fa148bd686a2527e34
Stephen Bradshaw <hello@stephenbradshaw.dev>
2023-01-12 13:56:17 +0000

    Add canvas scaling

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,15 +1,59 @@
-import { Draw } from "./types";
-import useCanvas from "./useCanvas";
+import {useEffect, useRef} from "react";
+import {Draw} from "./types";
 
 interface Props {
-  draw: Draw
+  draw: Draw;
 }
 
 const Canvas = (props: Props) => {
   const {draw, ...rest} = props;
-  const canvasRef = useCanvas({draw});
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  return <canvas ref={canvasRef} {...rest} />;
+  const resizeCanvas = (
+    canvas: HTMLCanvasElement,
+    ctx: CanvasRenderingContext2D
+  ) => {
+    const {width, height} = canvas.getBoundingClientRect();
+
+    if (canvas.width !== width || canvas.height !== height) {
+      const {devicePixelRatio: ratio = 1} = window;
+      canvas.width = width * ratio;
+      canvas.height = height * ratio;
+      ctx.scale(ratio, ratio);
+    }
+  };
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+    resizeCanvas(canvas, ctx);
+
+    let frameCount = 0;
+    let animationFrameId: number;
+
+    const render = () => {
+      frameCount++;
+      draw(ctx, frameCount);
+      animationFrameId = window.requestAnimationFrame(render);
+    };
+
+    render();
+
+    return () => {
+      window.cancelAnimationFrame(animationFrameId);
+    };
+  }, [draw]);
+
+  return (
+    <canvas
+      ref={canvasRef}
+      {...rest}
+      style={{width: "100vw", height: "100vh"}}
+    />
+  );
 };
 
 export default Canvas;
